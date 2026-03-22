b68229a751aa73727f433926fb0b8bdc32d5c6b5
horit-chan <horitaisgood@gmail.com>
2026-03-23 06:35:49 +0900

    タイトルを魔改造した！

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TodoApp from "../components/TodoApp";
 export default function Home() {
   return (
     <main style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <h1>🚀 Next.jsにお引越し完了！</h1>
+      <h1>🔥 MyTodoApp!!</h1>
       <TodoApp />
     </main>
   )
